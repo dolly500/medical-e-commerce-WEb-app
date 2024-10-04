d447ea6ae6ee7b6e8219539395d4f95d9075b5fa
Irsael <debanjo31@gmail.com>
2024-10-04 20:45:59 +0100

    updated gthe port

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -17,7 +17,7 @@ const sendMail = async (options) => {
   //     },
   // });
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: process.env.SMPT_SERVICE,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
