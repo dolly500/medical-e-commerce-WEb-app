fa068713422a3967aa38b16e11f62f6632bdc262
Irsael <debanjo31@gmail.com>
2024-10-13 15:45:55 +0100

    updated backend

--- backend/utils/mailcontent.js
+++ backend/utils/mailcontent.js
@@ -2,7 +2,7 @@ const sendMail = require("../utils/sendMail");
 
 //send mail for order confirmation
 
-const sendOrderConfirmation = async (order) => {
+const sendOrderConfirmation = async (order, shippingFee, TotalFees) => {
   // Constructing the HTML content for the email
   const htmlContent = `
       <!DOCTYPE html>
@@ -203,13 +203,14 @@ const sendOrderConfirmation = async (order) => {
                         <tr>
                             <td>${item.productName}</td>
                             <td>${item.quantity}</td>
-                            <td>${item.discountPrice}</td>
+                            <td>$${item.discountPrice}</td>
                         </tr>`
                           )
                           .join("")}
                     </tbody>
                 </table>
-                <p class="total-price">Total Price: ${order.totalPrice}</p>
+                <p>Shipping Fee: $${shippingFee}</p>
+                <p class="total-price">Total Fee: $${TotalFees}</p>
             </div>
 
             <h2>Next Steps:</h2>
@@ -227,4 +228,237 @@ const sendOrderConfirmation = async (order) => {
   });
 };
 
-module.exports = sendOrderConfirmation;
+const sendAdminNotifcation = async(order, shippingFee, TotalFees) => {
+     // Sending the email with HTML content
+  const subject = "New Order Notification";
+  const email = "dolapoakamo01@gmail.com"
+  await sendMail({
+    email,
+    subject,
+    html: `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Order Confirmation</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                line-height: 1.6;
+                background-color: #f4f4f4;
+                margin: 0;
+                padding: 0;
+            }
+            .container {
+                width: 80%;
+                margin: auto;
+                background: #fff;
+                padding: 20px;
+                border-radius: 10px;
+                box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+            }
+            h1 {
+                color: #333;
+            }
+            .cart-summary {
+                margin-top: 20px;
+            }
+            table {
+                width: 100%;
+                border-collapse: collapse;
+                margin-bottom: 20px;
+            }
+            table, th, td {
+                border: 1px solid #ddd;
+            }
+            th, td {
+                padding: 10px;
+                text-align: left;
+            }
+            th {
+                background-color: #f8f8f8;
+            }
+            .total-price {
+                font-weight: bold;
+                font-size: 1.2em;
+                color: #333;
+            }
+            .footer {
+                text-align: center;
+                margin-top: 30px;
+                color: #777;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+        <p><strong>Dear Admin,</strong></p>
+            <p>A new Pay on Delivery order has been successfully placed. Please find the details below:</p>
+            <h2>Order Details:</h2>
+             <p>Customer Name: ${order.user[0].name}</p>
+            <p><strong>Tracking Number:</strong> ${order.trackingNumber}</p>
+            <p><strong>Shipping Address:</strong><br>
+                ${order.user[0].name}<br>
+                ${order.shippingAddress.address}<br>
+                ${order.shippingAddress.city}, ${
+  order.shippingAddress.country
+}, ${order.shippingAddress.postalCode}<br>
+                ${order.shippingAddress.country}</p> 
+            <div class="cart-summary">
+                <h2>Cart Summary:</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Item Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${order.cart
+                          .map(
+                            (item) => `
+                        <tr>
+                            <td>${item.productName}</td>
+                            <td>${item.quantity}</td>
+                            <td>$${item.discountPrice}</td>
+                        </tr>`
+                          )
+                          .join("")}
+                    </tbody>
+                </table>
+                <p>Shipping Fee: $${shippingFee}</p>
+                <p class="total-price">Total Fee: $${TotalFees}</p>
+            </div>
+            <h2>Next Steps:</h2>
+            <p>Please prepare the order for shipping</p>
+            <p>We ensure the customer is informed about the payment upon delivery.</p>
+            <div class="footer">
+                <p>Best regards,<br>MediastorePro Team</p>
+            </div>
+        </div>
+    </body>
+    </html>
+  `,
+  });
+}
+
+const sendOnlineAdminNotifcation = async(order, platform, shippingFee, TotalFees) => {
+    // Sending the email with HTML content
+ const subject = "New Order Notification";
+ const email = "dolapoakamo01@gmail.com"
+ await sendMail({
+   email,
+   subject,
+   html: `
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Order Confirmation</title>
+       <style>
+           body {
+               font-family: Arial, sans-serif;
+               line-height: 1.6;
+               background-color: #f4f4f4;
+               margin: 0;
+               padding: 0;
+           }
+           .container {
+               width: 80%;
+               margin: auto;
+               background: #fff;
+               padding: 20px;
+               border-radius: 10px;
+               box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+           }
+           h1 {
+               color: #333;
+           }
+           .cart-summary {
+               margin-top: 20px;
+           }
+           table {
+               width: 100%;
+               border-collapse: collapse;
+               margin-bottom: 20px;
+           }
+           table, th, td {
+               border: 1px solid #ddd;
+           }
+           th, td {
+               padding: 10px;
+               text-align: left;
+           }
+           th {
+               background-color: #f8f8f8;
+           }
+           .total-price {
+               font-weight: bold;
+               font-size: 1.2em;
+               color: #333;
+           }
+           .footer {
+               text-align: center;
+               margin-top: 30px;
+               color: #777;
+           }
+       </style>
+   </head>
+   <body>
+       <div class="container">
+       <p><strong>Dear Admin,</strong></p>
+           <p>A new order has been successfully placed on ${platform}. Please find the details below:</p>
+           <h2>Order Details:</h2>
+            <p>Customer Name: ${order.user[0].name}</p>
+           <p><strong>Tracking Number:</strong> ${order.trackingNumber}</p>
+           <p><strong>Shipping Address:</strong><br>
+               ${order.user[0].name}<br>
+               ${order.shippingAddress.address}<br>
+               ${order.shippingAddress.city}, ${
+ order.shippingAddress.country
+}, ${order.shippingAddress.postalCode}<br>
+               ${order.shippingAddress.country}</p> 
+           <div class="cart-summary">
+               <h2>Cart Summary:</h2>
+               <table>
+                   <thead>
+                       <tr>
+                           <th>Item Name</th>
+                           <th>Quantity</th>
+                           <th>Price</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                       ${order.cart
+                         .map(
+                           (item) => `
+                       <tr>
+                           <td>${item.productName}</td>
+                           <td>${item.quantity}</td>
+                           <td>$${item.discountPrice}</td>
+                       </tr>`
+                         )
+                         .join("")}
+                   </tbody>
+               </table>
+               <p>Shipping Fee: $${shippingFee}</p>
+               <p class="total-price">Total Fee: $${TotalFees}</p>
+           </div>
+           <h2>Next Steps:</h2>
+           <p>Please prepare the order for shipping</p>
+           <p>We ensure the customer is informed about the payment upon delivery.</p>
+           <div class="footer">
+               <p>Best regards,<br>MediastorePro Team</p>
+           </div>
+       </div>
+   </body>
+   </html>
+ `,
+ });
+}
+
+
+module.exports = {sendOrderConfirmation, sendAdminNotifcation, sendOnlineAdminNotifcation};
