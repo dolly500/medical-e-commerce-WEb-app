08ad003920c4359f68588076e318b74bb477c119
“dolly500” <dolapoakamo01@gmail.com>
2024-10-28 07:16:46 +0100

    update

--- frontend/src/components/Profile/ProfileSidebar.jsx
+++ frontend/src/components/Profile/ProfileSidebar.jsx
@@ -44,30 +44,6 @@ const ProfileSidebar = ({ setActive, active }) => {
           Profile
         </span>
       </div>
-      {/* <div
-        className="flex items-center cursor-pointer w-full mb-8"
-        onClick={() => setActive(2)}
-      >
-        <HiOutlineShoppingBag size={20} color={active === 2 ? "red" : ""} />
-        <span
-          className={`pl-3 ${active === 2 ? "text-[red]" : ""
-            } 800px:block hidden`}
-        >
-          Orders
-        </span>
-      </div> */}
-      {/* <div
-        className="flex items-center cursor-pointer w-full mb-8"
-        onClick={() => setActive(3)}
-      >
-        <HiOutlineReceiptRefund size={20} color={active === 3 ? "red" : ""} />
-        <span
-          className={`pl-3 ${active === 3 ? "text-[red]" : ""
-            } 800px:block hidden`}
-        >
-          Refunds
-        </span>
-      </div> */}
 
       <div
         className="flex items-center cursor-pointer w-full mb-8"
