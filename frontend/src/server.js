137c8d34a024d5747b22aec62340f9583b46a8c7
“dolly500” <dolapoakamo01@gmail.com>
2024-11-04 23:47:36 +0100

    update

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://medical-e-commerce-web-app-bqh6.onrender.com/api/v2";
+export const server = "https://medical-e-commerce-web-app-4jhe.onrender.com/api/v2";
 
-export const servercl = "https://medical-e-commerce-web-app-bqh6.onrender.com";
+export const servercl = "https://medical-e-commerce-web-app-4jhe.onrender.com";
 
