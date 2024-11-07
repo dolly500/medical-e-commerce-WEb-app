6becbb1f7b58679e5228f6ced1830ba3330e0de2
“dolly500” <dolapoakamo01@gmail.com>
2024-11-07 09:35:14 +0100

    update

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:4000/api/v2";
+export const server = "https://medical-e-commerce-web-app-4jhe.onrender.com/api/v2";
 
-export const servercl = "http://localhost:4000";
+export const servercl = "https://medical-e-commerce-web-app-4jhe.onrender.com";
 
